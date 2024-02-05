2e513064be7a1354e12466b965ee83a134e437ab
Harpreet Singh Dhoot <55660057+harpreetsd99@users.noreply.github.com>
2024-02-04 22:23:08 -0800

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,6 +1,6 @@
 #credentials
 
-API_key = 'FrIT23XKYmTJv1yy20637fu10'
-API_key_secret = 'e2b3qdD1NRFRjhzBQNuhLahOHh7AzcaREJA3sdMOo7HpgzgTIL'
-Access_token = '1371813402952167429-fYlwD2RmE00mpeuhRrGemz7GdaDfJR'
-Access_token_secret = 'MgMYMy6vtDi3ZR5Bqlvsz4biVngAW2Md3eIUwCrg2Wi1j'
+API_key = ''
+API_key_secret = ''
+Access_token = ''
+Access_token_secret = ''
